4be5e9a81f67f8023b29e37885b285138dece7dd
earvin-tech <wsl@Earvin>
2025-04-19 05:07:09 +1000

    Fix: Import React in components and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-    }
-  },
-  base: '/',
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
